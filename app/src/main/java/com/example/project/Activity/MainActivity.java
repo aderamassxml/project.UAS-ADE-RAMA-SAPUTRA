95c3f63b205e5b66b86319dcc18163fc444e7a59
Ade Rama Saputra <ade.saputra@iconpln.co.id>
2024-01-12 22:48:57 +0700

    Testing

---
+++ app/src/main/java/com/example/project/Activity/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.project.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.project.Adapter.CategoryAdapter;
+import com.example.project.Adapter.PopularAdapter;
+import com.example.project.Domain.CategoryDomain;
+import com.example.project.Domain.FoodDomain;
+import com.example.project.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter, adapter2;
+    private RecyclerView recyclerViewCategoryList, recyclerViewPopularList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerViewCategory();
+        recyclerViewPopular();
+        bottomNavigation();
+    }
+
+    private void bottomNavigation() {
+        FloatingActionButton floatingActionButton = findViewById(R.id.card_btn);
+        LinearLayout homeBtn = findViewById(R.id.homeBtn);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CartListActivity.class));
+            }
+        });
+
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    private void recyclerViewPopular() {
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewPopularList = findViewById(R.id.recyclerView2);
+        recyclerViewPopularList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<FoodDomain> foodlist = new ArrayList<>();
+        foodlist.add(new FoodDomain("Ketupat Sayur", "ketupat", "Ketupat, dengan sayur nangka, sayur pakis, tersedia dengan telur dan tahu.", 5.3));
+        foodlist.add(new FoodDomain("Nasi Uduk", "nasiuduk", "Nasi uduk dengan bihun, tempe orek, telur, bawang goreng, dan kerupuk. ", 4.1));
+        foodlist.add(new FoodDomain("Keripik Singkong", "keripik", "Keripik singkong balado, dengan irisan daun jeruk", 2.0));
+
+        adapter2 = new PopularAdapter(foodlist);
+        recyclerViewPopularList.setAdapter(adapter2);
+
+    }
+
+    private void recyclerViewCategory() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewCategoryList = findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> categoryList = new ArrayList<>();
+        categoryList.add(new CategoryDomain("Nasi", "cat_1"));
+        categoryList.add(new CategoryDomain("Ketupat", "cat_2"));
+        categoryList.add(new CategoryDomain("Lupis", "cat_3"));
+        categoryList.add(new CategoryDomain("Minuman", "cat_4"));
+        categoryList.add(new CategoryDomain("Keripik", "cat_5"));
+
+        adapter = new CategoryAdapter(categoryList);
+        recyclerViewCategoryList.setAdapter(adapter);
+    }
+}
